2b7c752e35fd0ea163c451d23c2169aae6b366e8
javieku <javieku@gmail.com>
2018-09-04 21:44:08 +0200

    Fix parallel count change

--- week2/reductions/src/main/scala/reductions/ParallelCountChange.scala
+++ week2/reductions/src/main/scala/reductions/ParallelCountChange.scala
@@ -14,7 +14,7 @@ object ParallelCountChangeRunner {
     Key.exec.maxWarmupRuns -> 40,
     Key.exec.benchRuns -> 80,
     Key.verbose -> true
-  ) withWarmer(new Warmer.Default)
+  ) withWarmer (new Warmer.Default)
 
   def main(args: Array[String]): Unit = {
     val amount = 250
@@ -43,14 +43,14 @@ object ParallelCountChangeRunner {
 object ParallelCountChange {
 
   /** Returns the number of ways change can be made from the specified list of
-   *  coins for the specified amount of money.
-   */
+    * coins for the specified amount of money.
+    */
   def countChange(money: Int, coins: List[Int]): Int = {
     if (money < 0)
       return 0
     if (money == 0)
       return 1
-    if(coins.isEmpty)
+    if (coins.isEmpty)
       return 0
 
     if (coins.head > money)
@@ -62,28 +62,29 @@ object ParallelCountChange {
   type Threshold = (Int, List[Int]) => Boolean
 
   /** In parallel, counts the number of ways change can be made from the
-   *  specified list of coins for the specified amount of money.
-   */
+    * specified list of coins for the specified amount of money.
+    */
   def parCountChange(money: Int, coins: List[Int], threshold: Threshold): Int = {
-    if (threshold(money,coins)){
-       countChange(money,coins)
+    if (threshold(money, coins) || coins.isEmpty || money <= 0) {
+      countChange(money, coins)
     } else {
-      val (a, b) = parallel(countChange(money - coins.head, coins), countChange(money, coins.tail))
+      val (a, b) = parallel(parCountChange(money - coins.head, coins, threshold),
+        parCountChange(money, coins.tail, threshold))
       a + b
     }
   }
 
   /** Threshold heuristic based on the starting money. */
   def moneyThreshold(startingMoney: Int): Threshold =
-    ( money :Int, coins:List[Int] ) => money <= (startingMoney * 2) / 3
+    (money: Int, coins: List[Int]) => money <= (startingMoney * 2) / 3
 
   /** Threshold heuristic based on the total number of initial coins. */
   def totalCoinsThreshold(totalCoins: Int): Threshold =
-    ( money :Int, coins:List[Int] ) => coins.length <= (totalCoins * 2) / 3
+    (money: Int, coins: List[Int]) => coins.length <= (totalCoins * 2) / 3
 
 
   /** Threshold heuristic based on the starting money and the initial list of coins. */
   def combinedThreshold(startingMoney: Int, allCoins: List[Int]): Threshold = {
-    ( money :Int, coins:List[Int] ) => money*coins.length <= (allCoins.length * startingMoney * 2)
+    (money: Int, coins: List[Int]) => money * coins.length <= (allCoins.length * 0.5f * startingMoney)
   }
 }
--- week2/reductions/src/test/scala/reductions/ParallelCountChangeSuite.scala
+++ week2/reductions/src/test/scala/reductions/ParallelCountChangeSuite.scala
@@ -13,7 +13,7 @@ import ParallelCountChange._
 class ParallelCountChangeSuite extends FunSuite {
 
   test("countChange should return 0 for money < 0") {
-    def check(money: Int, coins: List[Int]) = 
+    def check(money: Int, coins: List[Int]) =
       assert(countChange(money, coins) == 0,
         s"countChang($money, _) should be 0")
 
@@ -24,7 +24,7 @@ class ParallelCountChangeSuite extends FunSuite {
   }
 
   test("countChange should return 1 when money == 0") {
-    def check(coins: List[Int]) = 
+    def check(coins: List[Int]) =
       assert(countChange(0, coins) == 1,
         s"countChang(0, _) should be 1")
 
@@ -34,7 +34,7 @@ class ParallelCountChangeSuite extends FunSuite {
   }
 
   test("countChange should return 0 for money > 0 and coins = List()") {
-    def check(money: Int) = 
+    def check(money: Int) =
       assert(countChange(money, List()) == 0,
         s"countChang($money, List()) should be 0")
 
@@ -63,5 +63,18 @@ class ParallelCountChangeSuite extends FunSuite {
     check(250, List(1, 2, 5, 10, 20, 50), 177863)
   }
 
+  test("parCountChange should invoke the parallel construct 6 times for money == 16, coins == List(1) and moneyThreshold(16)") {
+    def check(money: Int, coins: List[Int], threshold: Threshold, expected: Int) =
+      assert(parCountChange(money, coins, threshold) == expected,
+        s"countChange($money, $coins) should be $expected")
 
+    check(16, List(1), ParallelCountChange.moneyThreshold(16), 1)
+  }
+
+  test("combinedThreshold should return true when the number of coins times money is less than or equal " +
+    "to half of the initial number of coins times starting money") {
+    val threshold = ParallelCountChange.combinedThreshold(10, List(1,2,5,10))
+
+    assert(threshold(10,List(1)))
+  }
 }
