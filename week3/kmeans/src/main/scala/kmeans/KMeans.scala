41c0b1ffaff6ed2a5c55cc17f00b614e794ace20
javieku <javieku@gmail.com>
2018-10-09 18:25:44 +0200

    Implement k-means

--- week3/kmeans/src/main/scala/kmeans/KMeans.scala
+++ week3/kmeans/src/main/scala/kmeans/KMeans.scala
@@ -62,16 +62,18 @@ class KMeans {
   }
 
   def update(classified: GenMap[Point, GenSeq[Point]], oldMeans: GenSeq[Point]): GenSeq[Point] = {
-    ???
+    oldMeans.map( oldMean => findAverage(oldMean,classified(oldMean)))
   }
 
   def converged(eta: Double)(oldMeans: GenSeq[Point], newMeans: GenSeq[Point]): Boolean = {
-    ???
+    val accumulator: (Boolean, (Point, Point)) => Boolean = (acc, pair) => acc && (pair._1.squareDistance(pair._2) <= eta)
+    (oldMeans zip newMeans).aggregate(true) (accumulator, _&&_)
   }
 
   @tailrec
   final def kMeans(points: GenSeq[Point], means: GenSeq[Point], eta: Double): GenSeq[Point] = {
-    if (???) kMeans(???, ???, ???) else ??? // your implementation need to be tail recursive
+    val newMeans = update(classify(points,means), means)
+    if (!converged(eta)(means,newMeans)) kMeans(points, newMeans, eta) else newMeans // your implementation need to be tail recursive
   }
 }
 
